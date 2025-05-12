c9f273b3fecfd56f72fa79e51fb4263d66263f57
marcio lucas <lucasmrc2014@gmail.com>
2025-05-12 12:22:49 -0300

    first commit

---
+++ src/br/com/projeto/dao/IClienteDAO.java
@@ -0,0 +1,16 @@
+package br.com.projeto.dao;
+import br.com.projeto.domain.Cliente;
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+        public Boolean cadastrar(Cliente cliente);
+
+        public void excluir(Long cpf);
+
+        public void alterar(Cliente cliente);
+
+        public Cliente consultar(Long cpf);
+
+        public Collection<Cliente> buscarTodos();
+}
